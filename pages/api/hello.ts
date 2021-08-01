aa4c8c418667c6067843a60ace10072fcedde540
Karthikeyan <karthikeyan@bangthetable.com>
2021-08-01 22:22:18 +0530

    chore: seeding the firestore with forem articles 🔥

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import db from '../../utils/db'
 
 type Data = {
-  name: string
+  name: string,
+  batch: any
+}
+const headers: {'api-key'?: string } = {
+  'api-key': process.env.FOREM_API_KEY
+};
+
+const fetchFromForem = async () => {
+  const r = await fetch(`https://dev.to/api/articles/me`,{headers})
+  const data = await r.json()
+  return data
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  try{
+    var batch = db.batch();
+
+    const foremArticles = await fetchFromForem();
+     
+    foremArticles.forEach((doc: any) => {
+
+      batch.set(db.collection('col').doc(), doc);
+    })
+    
+    batch.commit()
+
+    res.status(200).json({ name: 'John Doe',batch })
+
+  }
+  catch(err){
+
+  }
 }
