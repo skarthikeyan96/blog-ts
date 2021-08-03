702d0d5159f58e95ebde9d37c30d39e23fae57db
Karthikeyan <karthikeyan@bangthetable.com>
2021-08-03 18:58:10 +0530

    feat: add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: {
+    enabled: true,
+    options: {
+      safelist: ['dark'], //specific classes
+    },
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}']
+  },
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
+    typography: (theme) => ({
+      dark: {
+        css: {
+          color: 'white'
+        }
+      }
+    }),
     extend: {},
   },
   variants: {
+          typography: ['dark'],
+    
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
