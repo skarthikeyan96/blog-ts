59d38a8ae2a5b6ff32c4590b5c35ae316c4a576e
Karthikeyan <karthikeyan@bangthetable.com>
2021-08-09 12:39:53 +0530

    feat: listing blog post in the /blog route

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -1,4 +1,3 @@
-// import fs from "fs"
 import matter from "gray-matter"
 import path from "path"
 import fs from "fs"
@@ -8,9 +7,11 @@ const getFrontMatter = () => {
   const files = fs.readdirSync(path.join(root, "data", "blog"))
 
   return files.reduce((allPosts: any, postSlug) => {
-    const source = fs.readFileSync(path.join(root, "data", "blog", postSlug), "utf8")
+    const source = fs.readFileSync(
+      path.join(root, "data", "blog", postSlug),
+      "utf8"
+    )
     const { data } = matter(source)
-
     return [
       {
         ...data,
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,12 +2,53 @@ import React from "react"
 import Layout from "../components/layout"
 import getFrontMatter from "../lib/mdx"
 
-type props = {}
+type props = {
+  posts: {
+    title: string
+    description: string
+    publishedAt: string
+    id: number
+    tags: string
+  }[]
+}
 
 const Blog: React.FC<props> = (props) => {
   return (
     <Layout>
-      <p> Blog </p>
+      <section className=" body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          {props.posts.map((post) => {
+            return (
+              <div
+                key={post.id}
+                className="-my-8 divide-y-2 divide-gray-100"
+              >
+                <div className="py-8 flex flex-wrap md:flex-nowrap">
+                  <div className="md:flex-grow">
+                    <h2 className="text-2xl font-medium  title-font mb-2">
+                      {post.title}
+                    </h2>
+                    <p className="pb-2">
+                      Published At :{" "}
+                      {new Date(post.publishedAt).toDateString()}
+                    </p>
+                    <p className="leading-relaxed">{post.description}</p>
+                    <ul className="text-gray-400 flex flex-wrap mt-2">
+                      {post.tags.split(",").map((tag, index) => {
+                        return (
+                          <li key={index} className="mr-3">
+                            #{tag}
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </section>
     </Layout>
   )
 }
